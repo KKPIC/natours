7807a981d50103c8da35b210547205582fd82a74
Choy Palubon <choypalubon@gmail.com>
2023-08-30 18:18:33 +0800

    Initial commit

---
+++ controllers/bookingController.js
@@ -0,0 +1,84 @@
+const Stripe = require('stripe');
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1 get the currently booked tour
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const tour = await Tour.findById(req.params.tourId);
+  // 2 create checkout session
+  const product = await stripe.products.create({
+    name: `${tour.name} Tour`,
+    description: tour.summary,
+    images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+  });
+
+  const price = await stripe.prices.create({
+    product: product.id,
+    unit_amount: tour.price * 100,
+    currency: 'usd',
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    mode: 'payment',
+    line_items: [
+      {
+        price: price.id,
+        quantity: 1,
+      },
+    ],
+  });
+
+  // 3 create session as a response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only temporary because it's unsecure: everyone  can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     success_url: `${req.protocol}://${req.get('host')}/`,
+//     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+//     customer_email: req.user.email,
+//     client_reference_id: req.params.tourID,
+//     line_items: [
+//       {
+//         price_data: {
+//           unit_amount: tour.price * 100,
+//           currency: 'usd',
+//           product_data: {
+//             name: `${tour.name} Tour`,
+//             images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+//             description: tour.summary,
+//           },
+//         },
+//         quantity: 1,
+//       },
+//     ],
+//     mode: 'payment',
+//   });
---
+++ public/js/stripe.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts.js';
+
+export const bookTour = async (tourId) => {
+  const stripe = Stripe(
+    'pk_test_51NkKenDTWh17KomRxzlE78cotNATyucEMkjfN5zSaLhC9UOEWaCmEuoUvl4a9jAwIbIehQPRDqZOwRbzCKAekSZh00cNYaOdxl'
+  );
+  try {
+    // 1 Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+    // 2 Create checkout form + charge card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
+
+// OLD
+// export const bookTour = async (tourId) => {
+//     try {
+//       // 1 Get checkout session from API
+//       const session = await axios(
+//         `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+//       );
+//       console.log(session);
+//       // 2 Create checkout form + charge card
+//       await stripe.redirectToCheckout({
+//         sessionId: session.data.session.id,
+//       });
+//     } catch (err) {
+//       console.log(err);
+//       showAlert('error', err);
+//     }
+//   };
